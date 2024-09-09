f922b41fc034883e75b343b9373d9ba3474235a3
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-09-09 12:09:53 +0300

    ok

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import base64
 from google.auth.exceptions import GoogleAuthError
 import time
 import ast
+import re
 # from googleapiclient.errors import HttpError
 
 app = Flask(__name__)
@@ -328,36 +329,49 @@ def webhook():
 
     elif poll_answer!="":
         ## retrieve chat id and what not
-        user_id=handle_poll_result(poll_answer)[0]
-        option=handle_poll_result(poll_answer)[1]
-        poll_id=handle_poll_result(poll_answer)[2]
-        ### reading the gsheet containing information about polling
-        polling_=read_gsheet(main_sheet_key, "Polling").cell(1, 1).value
-        value=ast.literal_eval(polling_)
-        project_id=value[str(poll_id)]
+        # user_id=handle_poll_result(poll_answer)[0]
+        # option=handle_poll_result(poll_answer)[1]
+        # poll_id=handle_poll_result(poll_answer)[2]
+        option=poll_answer['data']
+        user_id=poll_answer['message']['chat_id']
+        poll_id=poll_answer['message']['text']
+        # text=poll_answer[]
+        # callback_query.message.text
+        
+        pattern = r'\[([^\[\]]*)\]'
+        match = re.search(pattern, poll_id)
+        if match:
+            project_id=match.group(1)
 
-        ### reading the main sheet
-        main=read_gsheet(main_sheet_key, main_sheet_name)
-        main_content=pd.DataFrame(main.get_all_records())
-        key=list(main_content[main_content['project_id']==project_id]['key'])[0]
-        # time.sleep(1)
-        try:
-            enum=read_gsheet(key, "ENUM_LIST")
-            enum_df=pd.DataFrame(enum.get_all_records())
-            chat_ids=list(enum_df['CHAT_ID'])
-            Namez=list(enum_df['NAME'])
-            dict_=dict(zip(Namez, chat_ids))
-            # chat_id_alredy=dict_[Namez[option]]
-            if dict_[Namez[option]]!=None:
-                # send_message(chat_id, f"Some body {str(dict_[Namez[option]])} has already registered under <b>{Namez[option]}</b>! please, let {manager}/Bisrat know")
-            # a=list(pd.DataFrame(enum.get_all_records())['NAME'])[option]
-                send_message(user_id, "dfdf")
-            else:
-                enum.update_cell(option+2, 3, str(user_id))
-        except:
-            send_message(user_id, f"Some error please contact bisrat!")
-        ### updating the list based on the 
-        # send_message(user_id, f"you selected {option} for poll id {poll_id}")
+            ### reading the gsheet containing information about polling
+            # polling_=read_gsheet(main_sheet_key, "Polling").cell(1, 1).value
+            # value=ast.literal_eval(polling_)
+            # project_id=value[str(poll_id)]
+
+            ### reading the main sheet
+            main=read_gsheet(main_sheet_key, main_sheet_name)
+            main_content=pd.DataFrame(main.get_all_records())
+            key=list(main_content[main_content['project_id']==project_id]['key'])[0]
+            # time.sleep(1)
+            try:
+                enum=read_gsheet(key, "ENUM_LIST")
+                enum_df=pd.DataFrame(enum.get_all_records())
+                chat_ids=list(enum_df['CHAT_ID'])
+                Namez=list(enum_df['NAME'])
+                dict_=dict(zip(Namez, chat_ids))
+                # chat_id_alredy=dict_[Namez[option]]
+                if dict_[Namez[option]]!=None:
+                    # send_message(chat_id, f"Some body {str(dict_[Namez[option]])} has already registered under <b>{Namez[option]}</b>! please, let {manager}/Bisrat know")
+                # a=list(pd.DataFrame(enum.get_all_records())['NAME'])[option]
+                    send_message(user_id, "dfdf")
+                else:
+                    enum.update_cell(option+2, 3, str(user_id))
+            except:
+                send_message(user_id, f"Some error please contact bisrat!")
+            ### updating the list based on the 
+            # send_message(user_id, f"you selected {option} for poll id {poll_id}")
+        else:
+            send_message(user_id, "some error project id not found")
 
     return 'OK', 200
 

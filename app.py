1f0d2f67c4b37cfe2d8fd5aab9eec6544ede5894
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-09-17 22:22:10 +0300

    clean up

--- app.py
+++ app.py
@@ -453,7 +453,7 @@ def webhook():
             try:
                 daily_report=pd.DataFrame(read_gsheet(key, "Daily_Report").get_all_records())
                 # daily_report=daily_report[daily_report['CHAT_ID']==user_id]
-                send_message(user_id, "works till this point.")
+                # send_message(user_id, "works till this point.")
                 dates=list(set(list(daily_report['today'])))
                 print(dates)
                 print(option)
